067d3512777a201046b2c832a90a5b19031e3d2a
Severi Reivinen <severire>
2021-05-31 16:20:52 +0300

    Osa 3 3.9-3.11 tehty

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mooc-full-stack
+https://sleepy-sea-11396.herokuapp.com/api/persons
+https://sleepy-sea-11396.herokuapp.com/
--- osa3/index.js
+++ osa3/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 //app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('post', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
@@ -91,12 +94,12 @@ app.post('/api/persons', (req, res) => {
         number: body.number
     }
 
-    persons.concat(person)
+    persons = persons.concat(person)
 
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
