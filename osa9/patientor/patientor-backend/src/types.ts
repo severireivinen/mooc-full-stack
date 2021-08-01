28754722ec745f5fb17ab08e052b561bdf2e0f02
Severi Reivinen <severire>
2021-08-01 20:13:27 +0300

    Osa 9 9.19 tehty

--- osa9/patientor/patientor-backend/src/types.ts
+++ osa9/patientor/patientor-backend/src/types.ts
@@ -5,7 +5,34 @@ export interface Diagnose {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+export type Entry =
+    | HospitalEntry
+    | OccupationalHealthcareEntry
+    | HealthCheckEntry;
+
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeave;
+}
+
+export interface HospitalEntry extends BaseEntry {
+    type: 'Hospital';
+    discharge: Discharge;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+export interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnose['code']>;
 }
 
 export interface Patient {
@@ -22,6 +49,23 @@ export enum Gender {
     Male = 'male',
     Female = 'female',
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+type SickLeave = {
+    startDate: string;
+    endDate: string;
+};
+
+type Discharge = {
+    date: string;
+    criteria: string;
+};
 
 export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
 
